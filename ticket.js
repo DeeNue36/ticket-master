4a356848bf496f1c659861530a01030a6ab0354f
Dugiii <dugidemeg@gmail.com>
2025-07-20 02:59:12 -0500

    declaration of variables

---
+++ ticket.js
@@ -0,0 +1,11 @@
+const ticketApplicationContainer = document.querySelector('.ticket-application');
+const displayTicket = document.querySelector('.ticket-container');
+
+const ticketApplicationForm = document.querySelector('.ticket-form');
+const selectImage = document.querySelector('.upload-icon');
+const avatarInput = document.querySelector('#avatar');
+const nameInput = document.querySelector('#fullname');
+const emailInput = document.querySelector('#email');
+const githubInput = document.querySelector('#github-uname');
+
+const generateTicket = document.getElementById('generate-ticket');
