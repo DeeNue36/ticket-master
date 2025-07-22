4a76a407fce23153ec0e2c5fdfcdf4c9f7c1f31c
Dugiii <dugidemeg@gmail.com>
2025-07-21 21:39:24 -0500

    comments and displaying selected image

--- ticket.js
+++ ticket.js
@@ -1,6 +1,10 @@
+// * Get DOM Elements
+
+// * Sections to be displayed toggled
 const ticketApplicationContainer = document.querySelector('.ticket-application');
 const displayTicket = document.querySelector('.ticket-container');
 
+// * Ticket Application Form and Inputs
 const ticketApplicationForm = document.querySelector('.ticket-form');
 const selectImage = document.querySelector('.upload-icon');
 const avatarInput = document.querySelector('#avatar');
@@ -8,4 +12,22 @@ const nameInput = document.querySelector('#fullname');
 const emailInput = document.querySelector('#email');
 const githubInput = document.querySelector('#github-uname');
 
+// * Submit Button
 const generateTicket = document.getElementById('generate-ticket');
+
+
+selectImage.addEventListener('click', function(){
+    avatarInput.click();
+});
+
+avatarInput.addEventListener('change', function() {
+    const selectedFile = this.files[0];
+    const reader = new FileReader();
+
+    reader.onload = function(event) {
+        const img = document.querySelector('.upload-icon');
+        img.src = event.target.result;
+    };
+
+    reader.readAsDataURL(selectedFile);
+});
